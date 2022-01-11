984a30c74de65e1cf21fe564e97c32c7461c9f03
rodrigoagutierrez <82203115+rodrigoagutierrez@users.noreply.github.com>
2022-01-11 20:06:29 +0100

    cambios de estilos para formularios de usuarios

---
+++ controllers/loginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+  public static function login(Router $router) {
+    
+    $router->render('auth/login'); 
+  }
+  public static function logout() {
+    echo "Desde Logout";
+  }
+  public static function olvide(Router $router) {
+    $router->render('auth/olvide-password', [
+      
+    ]);
+  }
+  public static function recuperar() {
+    echo "Desde Reperar";
+  }
+  public static function crear(Router $router) {
+    $router->render('auth/crear-cuenta', [
+      
+    ]);
+  }
+}
